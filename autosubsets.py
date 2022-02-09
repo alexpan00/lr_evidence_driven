657db5b5bbce9a3a9156fadf70ee4584b081b80e
Alejandro <alexpan0098@gmail.com>
2022-02-09 12:49:56 +0100

    Generacion automatica del tamanyo de lso subsets

--- autosubsets.py
+++ autosubsets.py
@@ -6,7 +6,11 @@ import argparse
 from ast import parse
 from re import sub
 
+
 def count_transcripts(f_in:str)->int:
+    '''
+    Función que permite contar el número de tránscritos de un gtf
+    '''
     contador = 0
     with open (f_in, "r") as gtf:
         for linea in gtf:
@@ -15,15 +19,26 @@ def count_transcripts(f_in:str)->int:
     return contador
 
 def subset_sizes(total: int)-> list:
+    '''
+    Función que determian el tamaño de los subsets en función del número de
+    genes totales disponibles
+    '''
+    # Si hay menos de 1000 genes se cogen todos
     if total < 1000:
         subset = [total]
+    
+    # Si hay más de 1000 pero menos de 1500
     elif total > 1000 and total < 1500:
         subset = [200, 500, 800, 1000]
+    
+    # Si hay más de 1500 pero menos de 2000
     elif total > 1500 and total < 2000:
         subset = [200, 500, 800, 1000, 1500]
+    
+    # A partir de 2000 se inscrmenta de mil en mil hasta 8000
     elif total > 2000:
         subset = [200, 500, 800, 1000, 1500, 2000]
-        while (subset[-1] + 1000) < total:
+        while (subset[-1] + 1000) < total and (subset[-1] + 1000) <= 8000:
             subset.append(subset[-1] + 1000)
     return subset
 
@@ -35,9 +50,14 @@ def main():
                         help="size of the test set",
                         nargs='?', type=int, const=500, default=500)
     args = parser.parse_args()
+
+    # Contar los tránscritos
     total_transcripts = count_transcripts(args.gtf)
+
+    # Restar los tránscritos que se van a dedicar al testeo
     testing_transcripts = total_transcripts - args.test_size
     subsets = subset_sizes(testing_transcripts)
+    # printear la lista sin corchete para utilizarla en el script de bash
     print(*subsets, sep=",")
 
 if __name__=="__main__":
