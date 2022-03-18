2d1dd55fcb70e1ea029168f19d23e234f4802723
Alejandro <alexpan0098@gmail.com>
2022-03-18 16:44:49 +0100

    Script para generar hints a partir de gff

---
+++ gtf2hints.sh
@@ -0,0 +1,45 @@
+# Comprobar los argumentos que se le han pasado al script
+if [ -z "$1" ] || [ $1 == '-h' ]
+then
+    echo "Argumentos esperados:"
+    echo -e "\tgtf/gff"
+    echo -e "\tUTR (TRUE/FALSE)"
+    echo -e "\tSource (M,PB,...)"
+    echo -e "\tchromosome"
+    exit 0
+fi
+
+pwd;date
+# module load conda
+source activate busco
+
+# PATH
+utilities="/home/apadepe/utilities/"
+
+# Parámetros
+gff=$1      # gtf to be converted to hits
+utr=$2      # include or exclude utr
+src=$3      # Source of the information (M,PB)
+chr=$4      # Do for only one chromosome
+wd=$5
+name=$6
+if [ -z "$4" ]
+then
+    grep $chr $gff > tmp.gff
+    gff="tmp.gff"
+fi
+
+if [ "$utr"  == "TRUE" ]
+then
+    grep -P "\t(CDS|exon)\t" $gff | gtf2gff.pl --printIntron --out=tmp.gff
+    grep -P "\t(CDS|intron|exon)\t" tmp.gff > tmp2.gff
+else
+    grep -P "\t(CDS)\t" $gff | gtf2gff.pl --printIntron --out=tmp.gff
+    grep -P "\t(CDS|intron)\t" tmp.gff > tmp2.gff
+fi
+rm tmp.gff
+sed -i 's/gene_id \"/grp=/g' tmp2.gff
+cut -f1 -d'"' tmp2.gff | sed 's/$/;pri=1;src=M/g'
+mv tmp2 $name.hits.gff
+
+
