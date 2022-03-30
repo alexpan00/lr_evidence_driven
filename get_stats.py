6d677713dfbaa083d0e1c61dbd21686b9abe7a3c
Alejandro <alexpan0098@gmail.com>
2022-03-30 19:07:14 +0200

    Some more documentation

--- get_stats.py
+++ get_stats.py
@@ -1,6 +1,6 @@
 '''
-Script to reads the stats produce by cuffcompare for different files in the
-same directory and generate a file ith all the stats
+Script to read the stats produce by cuffcompare for different files in the
+same directory and generate a file with all the stats
 '''
 
 import argparse
@@ -11,13 +11,25 @@ import subprocess
 
 def parse_stats(f_in: str)-> str:
     '''
-    Read the stats file and collapse the important stats in one line
+    Function to read the stats file and collapse the important stats in one 
+    line. This stats are the sensitivity and precision at Nt, exon and gene
+    level.
+
+    Inputs:
+        f_in (str): path to the stats file produce by cuffcompare
+
+    Outputs:
+        stats_str (str): string with the important stats produce by cuffcompare
     '''
+    # Create a lsit to save the important stats produce by sufcompare
     stats = [0]*6
     with open(f_in, "r") as stats_file:
         for line in stats_file:
+            # The stats are in the lines that don't satart with #
             if not line.startswith("#") and len(line) > 1:
                 ll = line.split()
+                # the first fild of the line indicated the level
+                # The second the Sn and the third the Pr
                 if ll[0] == "Base":
                     stats[0] = ll[2]
                     stats[1] = ll[3]
@@ -28,30 +40,52 @@ def parse_stats(f_in: str)-> str:
                     stats[4] = ll[2]
                     stats[5] = ll[3]
                     break
+    # Convert the list to a str separated by \t
     stats_str = "\t".join(stats)
     return stats_str
 
 
 def parse_identity(f_in:str)-> str:
     '''
-    Read the file with the aligment info and get the number of perfect hits
-    the mean and the median identity
+    Function to read the file with the aligment info and get the number of
+    perfect hits, the mean and the median identity
+
+    Inputs:
+        f_in (str): path to the file with the alignmnet info
+    
+    Outputs:
+        identity_stats (str): get the summary stats from the aligment info
     '''
+    # Create a list to save the identity of all the aligments
     identity_list = list()
     with open(f_in, "r") as identity_file:
         for line in identity_file:
             identity_list.append(float(line.split()[2]))
+    
+    # Count the number of perfect hits (100 identity %)
     perfect_hits = str(identity_list.count(100.0))
     identity_median = str(statistics.median(identity_list))
     identity_mean = str(statistics.mean(identity_list))
+
+    # Create a str with the results
     identity_stats = identity_mean + "\t" + identity_median + "\t" + perfect_hits
 
     return identity_stats
 
 def get_FP(base_name: str)-> str:
     '''
-    Return the number of false positives, this is the number of genes that
-    could't be related to any gene in the reference
+    Function to obtain the number of false positives, this is the number of 
+    genes that could't be related to any gene in the reference.
+
+    This is done by comparing the number of lines in the tracking file produce
+    by cuffcompare and the number of lines in the file that contains the
+    aligments info.
+
+    Inputs:
+        base_name (str): name of the files wo/ the extension
+
+    Outputs:
+        false_positives (str): the total number of false positives
     '''
     # Count lines in the tracking file (total number of genes)
     tracking_file = subprocess.run(["wc", "-l", base_name + ".tracking"], 
@@ -70,17 +104,22 @@ def get_FP(base_name: str)-> str:
     false_positives = total_genes - mached_genes
     return str(false_positives)
 
-    
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("wd")
+    parser = argparse.ArgumentParser(description=("Get the stats produce by"
+                                                   " cuffcompare and the"
+                                                   " aligment"))
+    parser.add_argument("wd", help="path with the necessary files")
     args = parser.parse_args()
 
+    # get a list of the necessary files
     files = os.listdir(args.wd)
+
+    # Open the output file
     f_out = open(args.wd + "/summary.tsv", "w")
     f_out.write("N_genes\tnt_sn\tnt_sp\texon_sn\texon_sp\tgene_sn\tgene_sp\tmean_identity\tmedian_identity\tPH\tFP\n")
     for file in files:
+        # Get the stats output from cuffcompare
         if file.endswith(".stats"):
             # Get file basename
             base_name = file.split(".")[0]
